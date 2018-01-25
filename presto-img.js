217bd0669e9ce47995f36cc1079c1a54f8293f69
f1ux1uxday <fluxpatron@gmail.com>
2018-01-25 11:02:33 -0600

    initial commit

---
+++ presto-img.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const bing = require('node-bing-api')({
+  accKey: '3c4fc651-9bec-4934-9b5d-58433c870811',
+})
+
+const app = express()
+
+app.use(cors())
+
+app.get('/', (request, response) => {
+  response.send('to search, go to localhost:8080/img/your-keywords-here')
+})
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log('server is active.')
+})
